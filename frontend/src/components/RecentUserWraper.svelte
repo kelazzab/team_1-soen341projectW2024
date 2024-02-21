de573fccb606523708dc5823e0f0db2b2d71f694
apollinelbcr <apolline.leb@gmail.com>
2024-02-21 16:59:36 -0500

    test

---
+++ frontend/src/components/RecentUserWraper.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let user: string;
+    export let email: string;
+    
+    export let src: string;
+    export let href: string;
+</script>
+<a href={href} class="border border-gray-300 rounded-3xl w-1/2 md:w-1/3 lg:w-1/4 shrink-0 p-2 hover:scale-110 transition ease-in-out duration-700">
+    <img src={src} alt="Recent Users" class="rounded-t-3xl"/>
+    <hr/>
+    <div class="my-4">
+        <p>User: {user}/day</p>
+        <p>Email: {email}</p>
+    </div>
+</a>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<h2>Hello</h2>
+
+<script>
+    import RecentUserWraper from "$/Users/apolline/Documents/GitHub/team_1-soen341projectW2024/admin_panel/Dashboard/RecentUserWraper.svelte";
+</script>
+
+<Header/>
+
+<main class="md:px-10">
+
+    <RecentUserWraper>
+        <RecentUserWraper user="JohnSmith02" email="johnsmith2@gmail.com" href="/"/>
+        <RecentUserWraper user="Emma1234" email="emma_lopez@hotmail.com" href="/"/>
+        <RecentUserWraper user="AlexWilson3" email="alex.wilson@outlook.live" href="/"/>
+    </RecentUserWraper>
+
+</main>
